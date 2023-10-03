302fb10fbb8e0940143ca6c89c6bd7b035e0bb22
yunmokoo <yunmokoo7@gmail.com>
2023-10-02 23:20:24 -0700

    Add files via upload

---
+++ Lmint_chatbot.py
@@ -0,0 +1,37 @@
+
+import torch
+from peft import PeftModel    
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer, StoppingCriteria, StoppingCriteriaList, TextIteratorStreamer, BitsAndBytesConfig
+from transformers import AutoModel
+
+token = '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'
+
+model_id = 'meta-llama/Llama-2-7b-chat-hf'
+
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map='auto', token=token)
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=token)
+
+
+def sample_output(txt):
+    text = str(txt)
+    device = "cuda:0"
+
+    inputs = tokenizer(text, return_tensors="pt").to(device)
+    outputs = model.generate(**inputs, max_new_tokens=60)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+#sample_output('A good time to sleep is')
+
+
+'''
+The compute dtype is used to change the dtype that will be used during computation. 
+For example, hidden states could be in float32 but computation can be set to bf16 for speedups. 
+By default, the compute dtype is set to float32.
+'''
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model_cd_bf16 = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
